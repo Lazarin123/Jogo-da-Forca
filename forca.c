d695f4c3f3bb2dcc1e3fb183319d194acb51e58a
Samuel Lazarin <samuel.lazarin12@gmail.com>
2025-06-04 21:43:53 -0300

    Estruturação do jogo

--- forca.c
+++ forca.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
+void abertura () {
+    printf("***********************\n");
+    printf("*****Jogo da Forca*****\n");
+    printf("***********************\n");
+}
+
+void chuta(char chutes[26], int tentativas) {
+    char chute;
+    scanf(" %c", &chute);
+
+    chutes[tentativas] = chute;
+}
+
 int main () {
     char secretword[20];
     
@@ -11,5 +24,34 @@ int main () {
 
     char chutes[26];
     int tentativas = 0;
+
+    abertura ();
+
+    do {
+
+        for(int i = 0; i < strlen(secretword); i++) {
+
+            int achou = 0;
+
+            for (int j = 0; j < tentativas; j++) {
+                if (chutes[j] == secretword[i]) {
+                    achou = 1;
+                    break;
+                }
+            }
+
+            if (achou) {
+                printf("%c ", secretword[i]);
+            } else {
+            printf("_ ");
+            }
+
+        }
+        printf("\n");
+
+        chuta(chutes, tentativas);
+        tentativas++;
+
+    } while (!acertou && !enforcou);
 
 }
