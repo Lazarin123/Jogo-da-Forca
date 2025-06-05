b2a2fa3fdd99d5a9c777c09b4d5a1eb163bfb3e6
Samuel Lazarin <samuel.lazarin12@gmail.com>
2025-06-04 21:42:13 -0300

    estruturando

---
+++ forca.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+    char secretword[20];
+    
+    sprintf(secretword, "MELANCIA");
+
+    int acertou = 0;
+    int enforcou = 0;
+
+    char chutes[26];
+    int tentativas = 0;
+
+}
