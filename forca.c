de263dfd49ce6117fe3da7d82ba5704a7cdfda2c
Samuel Lazarin <128560645+Lazarin123@users.noreply.github.com>
2026-01-09 10:35:04 -0300

    Clean up forca.c by removing unused code
    Removed unused includes and variable declarations.

--- forca.c
+++ forca.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
-#include "forca.h"
 
-char secretword[TAMANHO_PALAVRA];
-char chutes[26];
-int chutesdados = 0;
 
 void abertura () {
     printf("***********************\n");
